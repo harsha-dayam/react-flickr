31b53f0b6e6151e2b932e98abeb911d7c0fae214
Harsha Dayam <harshavardhan2506@gmail.com>
2017-09-25 01:41:54 -0500

    Changes made to package.json and server.js

--- server.js
+++ server.js
@@ -1,4 +1,11 @@
-var express = require('express');
-var app = express();
-app.use('/', express.static(__dirname));
-app.listen(3000 || process.env.PORT , function(){ console.log('listening');});
+const express = require('express');
+const port = process.env.PORT || 8080;
+const app = express();
+
+app.use(express.static(__dirname + '/'));
+//////////////////////
+app.get('*', (req, res) =>{
+  res.sendFile(path.resolve(__dirname, 'index.html'));
+});
+//////////////////////
+app.listen(port);
